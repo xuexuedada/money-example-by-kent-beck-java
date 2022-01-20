beb250ef93be4672885dc2c33f1711b7d5d6ff67
Selvin Xue <xuexuedada@icloud.com>
2022-01-20 18:57:08 +0800

    Returned a new Dollar object from times() to fix its side-effect.

--- money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
+++ money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
@@ -10,10 +10,10 @@ class MoneyExampleTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
+        Dollar product = five.times(2);
         assertEquals(10, five.amount);
 
-        five.times(3);
+        product = five.times(3);
         assertEquals(15, five.amount);
 
     }
