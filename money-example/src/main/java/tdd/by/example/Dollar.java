5797f841f01b8a26f32461dfdcb5741502a3cb0b
Selvin Xue <xuexuedada@icloud.com>
2022-01-20 19:00:17 +0800

    Used Obvious Implementation to make the test to pass by returning a new Dollar object with the correct amount in method Dollar.times().

--- money-example/src/main/java/tdd/by/example/Dollar.java
+++ money-example/src/main/java/tdd/by/example/Dollar.java
@@ -10,7 +10,6 @@ public class Dollar {
     }
 
     public Dollar times(int multiplier) {
-        amount *= multiplier;
-        return null;
+        return new Dollar(amount * multiplier);
     }
 }
--- money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
+++ money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
@@ -11,10 +11,10 @@ class MoneyExampleTest {
     void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(10, product.amount);
 
         product = five.times(3);
-        assertEquals(15, five.amount);
+        assertEquals(15, product.amount);
 
     }
 }
