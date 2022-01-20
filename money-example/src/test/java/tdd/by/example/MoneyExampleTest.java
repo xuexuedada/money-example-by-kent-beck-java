bc3d178b95325d68f4848c011a19c5fabb22a9d2
Selvin Xue <xuexuedada@icloud.com>
2022-01-20 19:31:42 +0800

    Inlined temp variable in testMultiplication().

--- money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
+++ money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
@@ -10,14 +10,8 @@ class MoneyExampleTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-
-        assertEquals(new Dollar(10), product);
-
-        product = five.times(3);
-        assertEquals(15, product.amount);
-        assertEquals(new Dollar(15), product);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
 
     }
 
