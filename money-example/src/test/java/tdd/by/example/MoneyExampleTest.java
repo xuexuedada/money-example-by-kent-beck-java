36a2b02f7454ae99c44bc60d5774428551164019
Selvin Xue <xuexuedada@icloud.com>
2022-01-20 18:53:47 +0800

    Revealed the Dollar side effects in the test. Add a test case.

--- money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
+++ money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
@@ -12,6 +12,10 @@ class MoneyExampleTest {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+
+        five.times(3);
+        assertEquals(15, five.amount);
+
     }
 }
 
