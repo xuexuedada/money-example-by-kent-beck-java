a052fa2aaad050d8296464d15194d70d651bd67e
Selvin Xue <xuexuedada@icloud.com>
2022-01-21 14:05:23 +0800

    Made method Franc.times() call the static factory method of class Money instead of the constructor of the class Franc.

--- money-example/src/main/java/tdd/by/example/Franc.java
+++ money-example/src/main/java/tdd/by/example/Franc.java
@@ -10,6 +10,6 @@ public class Franc extends Money{
 
     @Override
     public Money times(int multiplier) {
-        return new Franc(amount * multiplier,null );
+        return Money.Franc(amount * multiplier);
     }
 }
