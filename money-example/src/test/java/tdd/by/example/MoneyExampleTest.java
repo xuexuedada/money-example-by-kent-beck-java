67326b2b6a7d224d7f31452de830ee817585f6b4
Selvin Xue <xuexuedada@icloud.com>
2022-01-21 13:49:02 +0800

    Used the factory method Money.dollar() everywhere in the tests.

--- money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
+++ money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
@@ -10,21 +10,21 @@ class MoneyExampleTest {
     @Test
     void testMultiplication() {
         Money five = Money.Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.Dollar(10), five.times(2));
+        assertEquals(Money.Dollar(15), five.times(3));
     }
 
     @Test
     void testEquality() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertNotEquals(new Dollar(5), new Dollar(6));
+        assertEquals(Money.Dollar(5), Money.Dollar(5));
+        assertNotEquals(Money.Dollar(5), Money.Dollar(6));
         assertEquals(new Franc(5), new Franc((5)));
         assertNotEquals(new Franc(5), new Franc((6)));
     }
 
     @Test
     void testCompareFrancsWithDollars() {
-        assertNotEquals(new Dollar(5), new Franc(5));
+        assertNotEquals(Money.Dollar(5), new Franc(5));
     }
 
     @Test
