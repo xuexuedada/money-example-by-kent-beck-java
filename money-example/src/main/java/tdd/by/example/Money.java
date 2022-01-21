f817aae7a9fabd9c726eff5fb0a79d5bfff563a7
Selvin Xue <xuexuedada@icloud.com>
2022-01-21 14:46:51 +0800

    Used getter instead of field in the method Money.equals().

--- money-example/src/main/java/tdd/by/example/Money.java
+++ money-example/src/main/java/tdd/by/example/Money.java
@@ -24,7 +24,7 @@ public class Money {
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency);
     }
 
     String currency() {
