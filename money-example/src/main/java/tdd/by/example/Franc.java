1cab17ec0080035bfcb18c7ff413aa67b76f2697
Selvin Xue <xuexuedada@icloud.com>
2022-01-21 14:38:41 +0800

    Changed method Franc.times() to return Money since it doesn't matter whether we have a Franc or Money.

--- money-example/src/main/java/tdd/by/example/Franc.java
+++ money-example/src/main/java/tdd/by/example/Franc.java
@@ -9,6 +9,6 @@ public class Franc extends Money{
 
     @Override
     public Money times(int multiplier) {
-        return new Money(amount * multiplier, currency);
+        return new Franc(amount * multiplier, currency);
     }
 }
